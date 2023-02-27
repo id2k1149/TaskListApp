18fc9f36bc062e1df27a9f9306c07f8c141834df
id2k1149 <id2k1149@gmail.com>
2023-02-27 10:46:18 -0500

    rename class ViewController to class TaskListViewController

--- TaskListApp/TaskListViewController.swift
+++ TaskListApp/TaskListViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
     }
-
-
 }
-
